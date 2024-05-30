04d889bd94745a5aa2b82286eee98dd52119746d
Jyotsan HAMAL <hamaljyotsan@gmail.com>
2024-05-30 21:21:36 +0545

    created api application, setting.py, models.py configured ,some middlewares

---
+++ backend/backend/test.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+print(STATIC_ROOT)
